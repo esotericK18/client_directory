a173129ceaba29fffc5439aa63ae0d3fc46eeae2
Rick Marcos <rmarcos1821@gmail.com>
2025-06-27 20:38:49 +0800

    Update to use clientService class and inject repo

--- lib/client/cli.rb
+++ lib/client/cli.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
-
 require_relative 'repositories/client_repository'
-require_relative 'services/search_clients_service'
-require_relative 'services/find_duplicate_emails_service'
+require_relative 'services/client_service'
 
 module Client
   # Main logic for the CLI
   class CLI
     def self.start(args)
+      repo = Repositories::ClientRepository.new
+      service = Services::ClientService.new(repo)
       command = args.shift
+      
       case command
       when 'search'
         query = args.join(' ')
-        results = Services::SearchClientsService.new.call(query)
+        results = service.find_by_partial_name(query)
         if results.empty?
           puts "No clients found with name containing '#{query}'."
         else
@@ -20,7 +21,7 @@ module Client
           results.each { |client| puts "#{client.name} (#{client.email})" }
         end
       when 'duplicates'
-        duplicates = Services::FindDuplicateEmailsService.new.call
+        duplicates = service.find_duplicates_by_email
         if duplicates.empty?
           puts 'No duplicates found.'
         else
